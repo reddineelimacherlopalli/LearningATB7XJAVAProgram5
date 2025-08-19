8321454f10d9a3df20d92eb926a7aa85127487ef
NEELIMA <reddineelimacrn@gmail.com>
2025-08-19 14:23:21 +0530

    Initial commit

---
+++ src/EX_190825/Lab003.java
@@ -0,0 +1,16 @@
+package EX_190825;
+
+public class Lab003 {
+    public static void main(String[] args) {
+
+        int a, b, c;
+        a = 7;
+        b = 6;
+        c = a * b;
+        if (c >= 10) {
+            System.out.println("Am big");
+        } else {
+            System.out.println("Am small");
+        }
+    }
+}
