323f01dc0be5421cf678ba8bdb259e808812194f
NEELIMA <reddineelimacrn@gmail.com>
2025-08-31 12:02:14 +0530

    todays programs 310825

--- src/EX_190825/Lab006.java
+++ src/EX_190825/Lab006.java
@@ -1,4 +1,13 @@
 package EX_190825;
 
 public class Lab006 {
+    public static void main(String[] args) {
+        String s="Neelima";
+        //String n= new String("Cherry");
+        s="Cherry";
+        s="Neel";
+        s="Neelima";
+        System.out.println(s);
+
+    }
 }
