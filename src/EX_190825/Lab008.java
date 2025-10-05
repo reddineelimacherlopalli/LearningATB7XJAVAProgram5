74a8dae71ca7e5506a28230393031b760e045785
NEELIMA <reddineelimacrn@gmail.com>
2025-10-05 14:55:54 +0530

    todays programs 310825

---
+++ src/EX_190825/Lab008.java
@@ -0,0 +1,14 @@
+package EX_190825;
+
+
+    public class Lab008 {
+        public static void main(String[] args) {
+            String s="MINISO";
+            //String n= new String("Cherry");
+            s="Cherry";
+            s="Neel";
+            s="MINISO";
+            System.out.println(s);
+
+        }
+    }
